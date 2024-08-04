e879f9a5d4772993a2ca5305d95068219e54bef9
unknown <chougulearvind1@gmail.com>
2024-08-04 04:27:49 -0700

    allow from any origin

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ app.use('/profile_img',express.static(path.join('profile_img')))
 app.use('/tweets',express.static(path.join('tweets')))
 app.use(morgan('dev'));
 app.use(cors({ 
-    origin: ['http://localhost:3000', 'https://dashing-sable-4fab70.netlify.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE','HEAD','PATCH'],
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
-    preflightContinue:true
+    'origin': ['http://localhost:3000', 'https://dashing-sable-4fab70.netlify.app'],
+   ' methods': ['GET', 'POST', 'PUT', 'DELETE','HEAD','PATCH'],
+    'credentials': true,
+    'allowedHeaders': ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+    
 }))
 app.use(express.json())
 app.use(bodyParser.json())
